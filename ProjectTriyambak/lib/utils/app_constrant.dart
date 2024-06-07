a5ed8ca5d193039f57ef450338b10e1e087589c9
div5268singh <107924737+div5268singh@users.noreply.github.com>
2024-06-08 03:07:53 +0530

    Update app_constrant.dart

--- ProjectTriyambak/lib/utils/app_constrant.dart
+++ ProjectTriyambak/lib/utils/app_constrant.dart
@@ -1,6 +1,6 @@
 // google ad ids
 
-const android_Google_banner = "ca-app-pub-3940256099942544/6300978111";
-const android_Google_interstitial = "ca-app-pub-3940256099942544/1033173712";
-const ios_Google_banner = "ca-app-pub-3940256099942544/2934735716";
-const ios_Google_interstitial = "ca-app-pub-3940256099942544/4411468910";
+const android_Google_banner = "";
+const android_Google_interstitial = "";
+const ios_Google_banner = "";
+const ios_Google_interstitial = "";
